3f78a4e88ba3165fe620c4ca9c23741896eb678f
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-10-17 13:46:11 +0100

    added sim_linelist tests

--- tests/testthat/test-sim_linelist.R
+++ tests/testthat/test-sim_linelist.R
@@ -21,8 +21,6 @@ suppressMessages({
   )
 })
 
-
-
 test_that("sim_list works as expected", {
   set.seed(1)
   linelist <- sim_linelist(
@@ -38,5 +36,72 @@ test_that("sim_list works as expected", {
     colnames(linelist),
     c("id", "gender", "age", "onset_date", "hospitalisation_date",
       "date_first_contact", "date_last_contact")
+  )
+})
+
+test_that("sim_list works as expected with age-strat rates", {
+  age_dep_hosp_rate <- data.frame(
+    min_age = c(1, 5, 80),
+    max_age = c(5, 80, 90),
+    rate = c(0.1, 0.05, 0.2)
+  )
+  age_dep_hosp_death_rate <- data.frame(
+    min_age = c(1, 5, 80),
+    max_age = c(5, 80, 90),
+    rate = c(0.1, 0.05, 0.2)
+  )
+  age_dep_non_hosp_death_rate <- data.frame(
+    min_age = c(1, 5, 80),
+    max_age = c(5, 80, 90),
+    rate = c(0.05, 0.025, 0.1)
+  )
+  set.seed(1)
+  linelist <- sim_linelist(
+    R = 1.1,
+    serial_interval = serial_interval,
+    onset_to_hosp = onset_to_hosp,
+    onset_to_death = onset_to_death,
+    hosp_rate = age_dep_hosp_rate,
+    hosp_death_rate = age_dep_hosp_death_rate,
+    non_hosp_death_rate = age_dep_non_hosp_death_rate
+  )
+
+  expect_s3_class(linelist, class = "data.frame")
+  expect_identical(dim(linelist), c(42L, 7L))
+  expect_identical(
+    colnames(linelist),
+    c("id", "gender", "age", "onset_date", "hospitalisation_date",
+      "date_first_contact", "date_last_contact")
+  )
+})
+
+test_that("sim_list works as expected with Ct", {
+  set.seed(1)
+  linelist <- sim_linelist(
+    R = 1.1,
+    serial_interval = serial_interval,
+    onset_to_hosp = onset_to_hosp,
+    onset_to_death = onset_to_death,
+    add_ct = TRUE
+  )
+
+  expect_s3_class(linelist, class = "data.frame")
+  expect_identical(dim(linelist), c(42L, 8L))
+  expect_identical(
+    colnames(linelist),
+    c("id", "gender", "age", "onset_date", "hospitalisation_date",
+      "date_first_contact", "date_last_contact", "ct_value")
+  )
+})
+
+test_that("sim_list fails as expected for include_contacts = TRUE while WIP", {
+  expect_error(
+    sim_linelist(
+      R = 1.1,
+      serial_interval = serial_interval,
+      onset_to_hosp = onset_to_hosp,
+      onset_to_death = onset_to_death, include_contacts = TRUE
+    ),
+    regexp = "Including contacts is not currently supported in \\{simulist\\}"
   )
 })
