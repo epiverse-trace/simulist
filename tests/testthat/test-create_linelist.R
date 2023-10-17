a4add43394db9a02f2d37f61ca9492ccb4628787
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-10-17 10:54:08 +0100

    added tests for .create_linelist

---
+++ tests/testthat/test-create_linelist.R
@@ -0,0 +1,89 @@
+test_that(".create_linelist works for pre_date_last_contact", {
+  linelist <- .create_linelist(scenario = "pre_date_last_contact")
+  expect_s3_class(linelist, class = "data.frame")
+  expect_identical(dim(linelist), c(42L, 8L))
+  expect_identical(
+    colnames(linelist),
+    c("n", "id", "infector", "generation", "time", "time_rounded", "onset_date",
+      "infector_time")
+  )
+})
+
+test_that(".create_linelist works for pre_date_first_contact", {
+  linelist <- .create_linelist(scenario = "pre_date_first_contact")
+  expect_s3_class(linelist, class = "data.frame")
+  expect_identical(dim(linelist), c(42L, 9L))
+  expect_identical(
+    colnames(linelist),
+    c("n", "id", "infector", "generation", "time", "time_rounded", "onset_date",
+      "infector_time", "date_last_contact")
+  )
+})
+
+test_that(".create_linelist works for pre_hospitalisation", {
+  linelist <- .create_linelist(scenario = "pre_hospitalisation")
+  expect_s3_class(linelist, class = "data.frame")
+  expect_identical(dim(linelist), c(42L, 12L))
+  expect_identical(
+    colnames(linelist),
+    c("n", "id", "infector", "generation", "time", "time_rounded", "onset_date",
+      "infector_time", "date_last_contact", "date_first_contact", "gender",
+      "age")
+  )
+})
+
+test_that(".create_linelist works for pre_deaths", {
+  linelist <- .create_linelist(scenario = "pre_deaths")
+  expect_s3_class(linelist, class = "data.frame")
+  expect_identical(dim(linelist), c(42L, 14L))
+  expect_identical(
+    colnames(linelist),
+    c("n", "id", "infector", "generation", "time", "time_rounded", "onset_date",
+      "infector_time", "date_last_contact", "date_first_contact", "gender",
+      "age", "hospitalisation", "hosp_rounded")
+  )
+})
+
+test_that(".create_linelist works for pre_names", {
+  linelist <- .create_linelist(scenario = "pre_names")
+  expect_s3_class(linelist, class = "data.frame")
+  expect_identical(dim(linelist), c(42L, 18L))
+  expect_identical(
+    colnames(linelist),
+    c("n", "id", "infector", "generation", "time", "time_rounded", "onset_date",
+      "infector_time", "date_last_contact", "date_first_contact", "gender",
+      "age", "hospitalisation", "hosp_rounded", "deaths", "death_rounded",
+      "hospitalisation_date", "death_date")
+  )
+})
+
+test_that(".create_linelist works for pre_ct", {
+  linelist <- .create_linelist(scenario = "pre_ct")
+  expect_s3_class(linelist, class = "data.frame")
+  expect_identical(dim(linelist), c(42L, 21L))
+  expect_identical(
+    colnames(linelist),
+    c("n", "id", "infector", "generation", "time", "time_rounded", "onset_date",
+      "infector_time", "date_last_contact", "date_first_contact", "gender",
+      "age", "hospitalisation", "hosp_rounded", "deaths", "death_rounded",
+      "hospitalisation_date", "death_date", "case_name", "infector_name",
+      "case_type")
+  )
+})
+
+test_that(".create_linelist fails as expected", {
+  expect_error(
+    .create_linelist(scenario = "random"),
+    regexp = "(arg)*(should be one of)"
+  )
+
+  expect_error(
+    .create_linelist(scenario = 1),
+    regexp = "(arg)*(must be)*(character)"
+  )
+
+  expect_error(
+    .create_linelist(),
+    regexp = "(argument)*(scenario)*(missing)"
+  )
+})
