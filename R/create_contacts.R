79763f8a366d8a6fddce133acfe7bdb44d795b8a
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-10-24 14:39:25 +0100

    updated create_contacts function to use add_cols functions and added arguments to allow flexibility

--- R/create_contacts.R
+++ R/create_contacts.R
@@ -1,95 +1,117 @@
-# this function is still WIP
-# nocov start
-# nolint start
-create_contacts <- function(.data, outbreak_start_date) {
+create_contacts <- function(.data,
+                            outbreak_start_date,
+                            contact_distribution,
+                            age_range,
+                            contact_tracing_status_probs = c(
+                              under_followup = 0.7,
+                              lost_to_followup = 0.2,
+                              unknown = 0.1
+                            ),
+                            config) {
+  contact_distribution <- epiparameter::epidist(
+    disease = "COVID-19",
+    epi_dist = "contact_distribution",
+    prob_distribution = "pois",
+    prob_distribution_params = c(l = 5)
+  )
 
-  if (!"infector_name" %in% colnames(linelist)) {
-    linelist <- .add_names(.data = linelist)
+  if (!"infector_name" %in% colnames(.data)) {
+    .data <- .add_names(.data = .data)
   }
 
   contact_investigation <- subset(
-    linelist,
+    .data,
     select = c(
-      "infector_name", "case_name", "age", "gender", "date_first_contact",
-      "date_last_contact")
+      "infector", "infector_name", "case_name", "age", "gender",
+      "date_first_contact", "date_last_contact")
   )
   colnames(contact_investigation) <- c(
-    "part_name", "contact_name", "cnt_age", "cnt_gender",
+    "infector", "part_name", "contact_name", "cnt_age", "cnt_gender",
     "date_first_contact", "date_last_contact"
   )
   contact_investigation <- contact_investigation[-1, ]
   contact_investigation$was_case <- "Y"
 
-  other_contacts <- subset(linelist, select = c("infector", "infector_time"))
+  other_contacts <- subset(.data, select = c("infector", "infector_time"))
   other_contacts <- other_contacts[-1, ]
   other_contacts$part_name <- contact_investigation$part_name
-  other_contacts <- subset(other_contacts, subset = !duplicated(infector))
-
-  #Add a frequency of repetition for each infector V
-  other_contacts$freq <- sample(x = 1:10, size = nrow(other_contacts), replace = TRUE)
-
-
-  #Multiply row by times specified in frequency column V
-  other_contacts <- other_contacts[rep(row.names(other_contacts), other_contacts$freq), 1:3]
-
-  #Add random ages V
-  other_contacts$cnt_age <- sample(0:90, replace = TRUE, size = length(other_contacts$infector))
-
-  #Add random genders V
-  other_contacts$cnt_gender <- sample(c("m", "f"), size = nrow(other_contacts), replace = TRUE)
-
-  #Add corresponding names acc to the gender V
-  cases_m <- sum(other_contacts$cnt_gender == "m")
-  cases_f <- sum(other_contacts$cnt_gender == "f")
-  names <- list(
-    names_masc = randomNames::randomNames(
-      n = cases_m,
-      which.names = 'both',
-      name.sep = " ",
-      name.order = "first.last",
-      gender = 0,
-      sample.with.replacement = FALSE
-    ),
-    names_fem = randomNames::randomNames(
-      n = cases_f,
-      which.names = 'both',
-      name.sep = " ",
-      name.order = "first.last",
-      gender = 1,
-      sample.with.replacement = FALSE
-    )
+  other_contacts <- subset(
+    other_contacts,
+    subset = !duplicated(other_contacts$infector)
   )
 
-  other_contacts$contact_name[other_contacts$cnt_gender == "m"] <- names$names_m # move to start of df
-  other_contacts$contact_name[other_contacts$cnt_gender == "f"] <- names$names_f
+  # sample contact distribution for non-infected contacts
+  contact_freq <- epiparameter::generate(
+    contact_distribution,
+    nrow(other_contacts)
+  )
 
+  #Multiply row by times specified in frequency column V
+  other_contacts <-
+    other_contacts[rep(row.names(other_contacts), contact_freq), ]
 
-  #Adding contact dates
-  other_contacts$date_last_contact <- other_contacts$infector_time + stats::rpois(nrow(other_contacts),3) + outbreak_start_date
-  other_contacts$date_first_contact <- other_contacts$date_last_contact - stats::rpois(nrow(other_contacts),3)
-  other_contacts <- other_contacts[,c(3,6,4,5,7,8)]
-  other_contacts$was_case <- 'N'
+  # add random age and gender
+  other_contacts$cnt_age <- sample(
+    age_range[1]:age_range[2],
+    replace = TRUE,
+    size = nrow(other_contacts)
+  )
+  other_contacts$cnt_gender <- sample(
+    c("m", "f"),
+    size = nrow(other_contacts),
+    replace = TRUE
+  )
 
+  #Add corresponding names acc to the gender V
+  other_contacts$contact_name <- randomNames::randomNames(
+    which.names = "both",
+    name.sep = " ",
+    name.order = "first.last",
+    gender = other_contacts$cnt_gender,
+    sample.with.replacement = FALSE
+  )
+
+  # add contact dates
+  other_contacts <- .add_date_last_contact(
+    .data = other_contacts,
+    outbreak_start_date = outbreak_start_date,
+    distribution = config$last_contact_distribution,
+    config$last_contact_distribution_params
+  )
+  other_contacts <- .add_date_first_contact(
+    .data = other_contacts,
+    distribution = config$first_contact_distribution,
+    config$first_contact_distribution_params
+  )
+
+  other_contacts <- other_contacts[, c(1, 3, 6, 4, 5, 8, 7)]
+  other_contacts$was_case <- "N"
 
   # merge both datasets and keep all the values from both
-  for (i in seq_len(nrow(other_contacts))) {
-    contact_investigation_names <- contact_investigation$part_name
-    if (other_contacts[i, "part_name"] %in% contact_investigation_names) {
-      last_row <- max(which(contact_investigation_names == other_contacts[i, "part_name"]))
-      contact_investigation <- rbind(contact_investigation[1:last_row, ], other_contacts[i, ], contact_investigation[-(1:last_row), ])
-    }
-  }
+  contact_investigation <- rbind(contact_investigation, other_contacts)
+  contact_investigation <-
+    contact_investigation[order(contact_investigation$infector), ]
+  row.names(contact_investigation) <- NULL
 
-  ###Adding a contact tracing status ####
-  status_list <- c("under_followup", "lost_to_followup", "unknown")
+  # add contact tracing status
   pick_N <- which(contact_investigation$was_case == "N")
-  status_type <- sample(status_list, length(pick_N), replace=TRUE, prob=c(0.7,0.2,0.1))
+  status_type <- sample(
+    names(contact_tracing_status_probs),
+    size = length(pick_N),
+    replace = TRUE,
+    prob = contact_tracing_status_probs
+  )
   contact_investigation$status <- NA
   contact_investigation$status[pick_N] <- status_type
-  contact_investigation$status <- ifelse(is.na(contact_investigation$status), 'case', contact_investigation$status)
+  contact_investigation$status <- ifelse(
+    test = is.na(contact_investigation$status),
+    yes = "case",
+    no = contact_investigation$status
+  )
+
+  # remove infector col
+  contact_investigation <- contact_investigation[, -1]
 
   # return contacts
   contact_investigation
 }
-# nolint end
-# nocov end
