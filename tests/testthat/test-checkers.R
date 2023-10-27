048ef28fc10a4dd121a06cf68e0fa053cf2f5c3e
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-10-27 16:09:01 +0100

    updated tests for new rate df

--- tests/testthat/test-checkers.R
+++ tests/testthat/test-checkers.R
@@ -1,30 +1,30 @@
 test_that(".check_rate_df works as expected", {
   age_dep_hosp_rate <- data.frame(
-    min_age = c(1, 5, 80),
-    max_age = c(5, 80, 90),
+    age_limit = c(1, 5, 80),
     rate = c(0.1, 0.05, 0.2)
   )
   age_dep_hosp_rate <- .check_rate_df(age_dep_hosp_rate, age_range = c(1, 90))
   expect_s3_class(age_dep_hosp_rate, class = "data.frame")
   expect_identical(dim(age_dep_hosp_rate), c(3L, 3L))
   expect_identical(colnames(age_dep_hosp_rate), c("min_age", "max_age", "rate"))
-  expect_identical(rownames(age_dep_hosp_rate), c("[1,5)", "[5,80)", "[80,90]"))
+  expect_identical(
+    row.names(age_dep_hosp_rate),
+    c("[1,5)", "[5,80)", "[80,90]")
+  )
 })
 
 test_that(".check_rate_df fails as expected", {
   age_dep_hosp_rate <- data.frame(
     age_min = c(1, 5, 80),
-    age_max = c(5, 80, 90),
     hosp_rate = c(0.1, 0.05, 0.2)
   )
   expect_error(
     .check_rate_df(age_dep_hosp_rate, age_range = c(1, 91)),
-    regexp = "column names should be 'min_age', 'max_age' & 'rate'"
+    regexp = "column names should be 'age_limit' & 'rate'"
   )
 
   age_dep_hosp_rate <- data.frame(
-    min_age = c(2, 5, 80),
-    max_age = c(5, 80, 90),
+    age_limit = c(2, 5, 80),
     rate = c(0.1, 0.05, 0.2)
   )
   expect_error(
@@ -33,18 +33,17 @@ test_that(".check_rate_df fails as expected", {
   )
 
   age_dep_hosp_rate <- data.frame(
-    min_age = c(1, 5, 80),
-    max_age = c(5, 80, 90),
+    age_limit = c(1, 5, 90),
     rate = c(0.1, 0.05, 0.2)
   )
   expect_error(
-    .check_rate_df(age_dep_hosp_rate, age_range = c(1, 91)),
-    regexp = "maximum age of the highest age group should match upper age range"
+    .check_rate_df(age_dep_hosp_rate, age_range = c(1, 90)),
+    regexp =
+      "lower bound of oldest age group must be lower than highest age range"
   )
 
   age_dep_hosp_rate <- data.frame(
-    min_age = c(1, 5, 80),
-    max_age = c(5, 80, 90),
+    age_limit = c(1, 5, 80),
     rate = c(-0.1, 0.5, 1.1)
   )
   expect_error(
@@ -53,22 +52,20 @@ test_that(".check_rate_df fails as expected", {
   )
 
   age_dep_hosp_rate <- data.frame(
-    min_age = c(1, 5, 60),
-    max_age = c(10, 70, 90),
+    age_limit = c(1, 5, 5),
     rate = c(0.1, 0.05, 0.2)
   )
   expect_error(
     .check_rate_df(age_dep_hosp_rate, age_range = c(1, 90)),
-    regexp = "age groups should be non-overlapping"
+    regexp = "age limit in rate data frame must be unique"
   )
 
   age_dep_hosp_rate <- data.frame(
-    min_age = c(1, 10, 80),
-    max_age = c(3, 60, 90),
-    rate = c(0.1, 0.05, 0.2)
+    age_limit = c(1, 10, 80),
+    rate = c(0.1, 0.05, NA)
   )
   expect_error(
     .check_rate_df(age_dep_hosp_rate, age_range = c(1, 90)),
-    regexp = "age groups should be contiguous"
+    regexp = "age limit or rate cannot be NA or NaN"
   )
 })
