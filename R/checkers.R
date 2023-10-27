ffc4c7c1562e52df08efe1f129bf4dc61309d687
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-10-27 16:00:08 +0100

    updated .check_rate_df to use new user input rate df

--- R/checkers.R
+++ R/checkers.R
@@ -7,44 +7,42 @@
 #' invalid.
 #' @keywords internal
 .check_rate_df <- function(x, age_range) {
-
-  # get vector of all age groups
-  age_groups <- apply(x, 1, function(y) y[1]:y[2])
-
-  # remove last integer from bracket due to exclusive upper bound
-  # oldest age bracket has inclusive upper bound
-  age_groups_ <- lapply(
-    age_groups,
-    FUN = function(x) x[-length(x)]
-  )
-  age_groups_[length(age_groups)] <- age_groups[length(age_groups)]
-  age_vec <- unlist(age_groups_)
-
+  # check input
   stopifnot(
-    "column names should be 'min_age', 'max_age' & 'rate'" =
-      c("min_age", "max_age", "rate") %in% colnames(x),
+    "column names should be 'age_limit' & 'rate'" =
+      setequal(c("age_limit", "rate"), colnames(x)),
     "minimum age of lowest age group should match lower age range" =
-      age_range[1] == min(x$min_age),
-    "maximum age of the highest age group should match upper age range" =
-      age_range[2] == max(x$max_age),
+      age_range[1] == min(x$age_limit),
+    "lower bound of oldest age group must be lower than highest age range" =
+      age_range[2] > max(x$age_limit),
+    "age limit or rate cannot be NA or NaN" =
+      !anyNA(x),
     "rate should be between 0 and 1" =
       min(x$rate) >= 0 & max(x$rate) <= 1,
-    "age groups should be non-overlapping" =
-      !anyDuplicated(age_vec) > 0,
-    "age groups should be contiguous" =
-      all(age_range[1]:age_range[2] %in% unique(age_vec))
+    "age limit in rate data frame must be unique" =
+      !(anyDuplicated(x$age_limit) > 0)
   )
 
-  rownames(x) <- paste0(
-    "[", x$min_age, ",", x$max_age, ")"
-  )
+  # order rate df on age_limit
+  x <- x[order(x$age_limit), ]
 
+  # format rate data frame
+  age_range_ <- age_range[1]:age_range[2]
+  # findInterval inclusive/exclusive bound rules match age bracket
+  age_groups <- unname(split(age_range_, findInterval(age_range_, x$age_limit)))
+
+  # add and sort data frames cols
+  x$max_age <- vapply(age_groups, max, FUN.VALUE = numeric(1))
+  colnames(x) <- c("min_age", "rate", "max_age")
+  x <- x[, c("min_age", "max_age", "rate")]
+
+  # add informative row names
+  row.names(x) <- paste0(
+    "[", x$min_age, ",", (x$max_age + 1), ")"
+  )
   # last age bracket has inclusive upper bound
-  rownames(x)[nrow(x)] <- gsub(
-    pattern = ")",
-    replacement = "]",
-    x = rownames(x)[nrow(x)],
-    fixed = TRUE
+  row.names(x)[nrow(x)] <- paste0(
+    "[", x$min_age[nrow(x)], ",", x$max_age[nrow(x)], "]"
   )
 
   # return rate data frame
