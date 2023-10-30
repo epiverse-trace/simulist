49cf0096ea4304ebb8e9f61da6f82fa69df8d74e
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-10-30 16:42:36 +0000

    added tests for .check_age_df

--- tests/testthat/test-checkers.R
+++ tests/testthat/test-checkers.R
@@ -69,3 +69,67 @@ test_that(".check_rate_df fails as expected", {
     regexp = "age limit or rate cannot be NA or NaN"
   )
 })
+
+test_that(".check_age_df works as expected", {
+  age_struct <- data.frame(
+    age_range = c("1-4", "5-79", "80-90"),
+    proportion = c(0.1, 0.7, 0.2),
+    stringsAsFactors = FALSE
+  )
+  age_struct <- .check_age_df(age_struct)
+  expect_s3_class(age_struct, class = "data.frame")
+  expect_identical(dim(age_struct), c(3L, 3L))
+  expect_identical(colnames(age_struct), c("min_age", "max_age", "proportion"))
+})
+
+test_that(".check_age_df fails as expected", {
+  age_struct <- data.frame(
+    age_range = c("1-4", "5-79", "80-90"),
+    percentage = c(0.1, 0.7, 0.2),
+    stringsAsFactors = FALSE
+  )
+  expect_error(
+    .check_age_df(age_struct),
+    regexp = "column names should be 'age_range' & 'proportion'"
+  )
+
+  age_struct <- data.frame(
+    age_range = c("1-4", "5-79", "80-90"),
+    proportion = c(0.1, NA, 0.2),
+    stringsAsFactors = FALSE
+  )
+  expect_error(
+    .check_age_df(age_struct),
+    regexp = "age range or proportion cannot be NA or NaN"
+  )
+
+  age_struct <- data.frame(
+    age_range = c("1-4", "5-79", "80-90"),
+    proportion = c(0.1, 0.7, 0.3),
+    stringsAsFactors = FALSE
+  )
+  expect_error(
+    .check_age_df(age_struct),
+    regexp = "proportions of each age bracket should sum to 1"
+  )
+
+  age_struct <- data.frame(
+    age_range = c("1_4", "5-79", "80-90"),
+    proportion = c(0.1, 0.7, 0.2),
+    stringsAsFactors = FALSE
+  )
+  expect_error(
+    .check_age_df(age_struct),
+    regexp = "(all age groups should be separated with a)*(-)"
+  )
+
+  age_struct <- data.frame(
+    age_range = c("1-4", "52-79", "80-90"),
+    proportion = c(0.1, 0.7, 0.2),
+    stringsAsFactors = FALSE
+  )
+  expect_error(
+    .check_age_df(age_struct),
+    regexp = "age groups should be contiguous"
+  )
+})
