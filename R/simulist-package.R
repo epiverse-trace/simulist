6e9cce7b83abdbcdaf3c90567ec0c6a3e4c6dff7
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-11-29 16:52:40 +0000

    added package documentation with func to remove unused imports check note

---
+++ R/simulist-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+# nocov start
+# fix for R CMD check from https://github.com/hadley/r-pkgs/issues/828
+ignore_unused_imports <- function() {
+  epiparameter::epidist
+}
+# nocov end
