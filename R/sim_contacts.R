0b629efc0b0121814ef4e38c47f9c19970b87533
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-01-17 09:43:40 +0000

    removed contact_distribution from functions that no longer use it

--- R/sim_contacts.R
+++ R/sim_contacts.R
@@ -1,9 +1,6 @@
 #' Simulate contacts for an infectious disease outbreak
 #'
 #' @inheritParams sim_linelist
-#' @param contact_distribution An `<epidist>` object or anonymous function for
-#' the contact distribution. This is similar to the offspring distribution,
-#' but describes the heterogeneity of contacts that are not infected.
 #' @param contact_tracing_status_probs A named `numeric` vector with the
 #' probability of each contact tracing status. The names of the vector must
 #' be `"under_followup"`, `"lost_to_followup"`, `"unknown"`. Values of each
@@ -37,7 +34,6 @@
 sim_contacts <- function(mean_contacts,
                          contact_interval,
                          prob_infect,
-                         contact_distribution,
                          outbreak_start_date = as.Date("2023-01-01"),
                          min_outbreak_size = 10,
                          population_age = c(1, 90),
@@ -50,14 +46,9 @@ sim_contacts <- function(mean_contacts,
   # check and convert distribution to func if needed before .check_sim_input()
   stopifnot(
     "Input delay distributions need to be either functions or <epidist>" =
-      inherits(contact_interval, c("function", "epidist")) &&
-      inherits(contact_distribution, c("function", "epidist"))
+      inherits(contact_interval, c("function", "epidist"))
   )
   contact_interval <- as.function(serial_interval, func_type = "generate")
-  contact_distribution <- as.function(
-    contact_distribution,
-    func_type = "generate"
-  )
 
   .check_sim_input(
     sim_type = "contacts",
