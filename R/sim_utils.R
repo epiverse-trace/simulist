91f77ff92bd3a2ecb3d60305f89c271a4827fdfe
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-02-06 10:28:48 +0000

    removed add_names argument from .sim_bp_linelist

--- R/sim_utils.R
+++ R/sim_utils.R
@@ -20,7 +20,6 @@ NULL
                              outbreak_start_date,
                              min_outbreak_size,
                              population_age,
-                             add_names,
                              config) {
   outbreak_size <- 0
   max_iter <- 0L
@@ -30,7 +29,7 @@ NULL
       contact_distribution = contact_distribution,
       contact_interval = contact_interval,
       prob_infect = prob_infect,
-      add_names = add_names
+      config = config
     )
     outbreak_size <- sum(chain$infected == "infected")
     max_iter <- max_iter + 1L
