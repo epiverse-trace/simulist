05a7fc9e9dc4a2783a5a3b1d7cfab2b4948ec068
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-04-10 17:55:07 +0100

    updated create_config tests

--- tests/testthat/test-create_config.R
+++ tests/testthat/test-create_config.R
@@ -1,13 +1,14 @@
 test_that("create_config works as expected with defaults", {
   config <- create_config()
   expect_type(config, type = "list")
-  expect_length(config, 7)
+  expect_length(config, 8)
   expect_named(
     config,
     c(
       "last_contact_distribution", "last_contact_distribution_params",
       "first_contact_distribution", "first_contact_distribution_params",
-      "ct_distribution", "ct_distribution_params", "network"
+      "ct_distribution", "ct_distribution_params", "network",
+      "time_varying_death_risk"
     )
   )
 })
@@ -15,13 +16,14 @@ test_that("create_config works as expected with defaults", {
 test_that("create_config works as expected modifying element", {
   config <- create_config(last_contact_distribution = "geom")
   expect_type(config, type = "list")
-  expect_length(config, 7)
+  expect_length(config, 8)
   expect_named(
     config,
     c(
       "last_contact_distribution", "last_contact_distribution_params",
       "first_contact_distribution", "first_contact_distribution_params",
-      "ct_distribution", "ct_distribution_params", "network"
+      "ct_distribution", "ct_distribution_params", "network",
+      "time_varying_death_risk"
     )
   )
   expect_identical(config$last_contact_distribution, "geom")
@@ -35,13 +37,14 @@ test_that("create_config works as expected with spliced list", {
     )
   )
   expect_type(config, type = "list")
-  expect_length(config, 7)
+  expect_length(config, 8)
   expect_named(
     config,
     c(
       "last_contact_distribution", "last_contact_distribution_params",
       "first_contact_distribution", "first_contact_distribution_params",
-      "ct_distribution", "ct_distribution_params", "network"
+      "ct_distribution", "ct_distribution_params", "network",
+      "time_varying_death_risk"
     )
   )
   expect_identical(config$ct_distribution, "lnorm")
@@ -55,13 +58,14 @@ test_that("create_config works as expected with spliced list", {
     )
   )
   expect_type(config, type = "list")
-  expect_length(config, 7)
+  expect_length(config, 8)
   expect_named(
     config,
     c(
       "last_contact_distribution", "last_contact_distribution_params",
       "first_contact_distribution", "first_contact_distribution_params",
-      "ct_distribution", "ct_distribution_params", "network"
+      "ct_distribution", "ct_distribution_params", "network",
+      "time_varying_death_risk"
     )
   )
   expect_identical(config$last_contact_distribution, "geom")
