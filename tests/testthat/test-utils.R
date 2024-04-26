0a54facb8489ebf95ed758b89c167296b9962d19
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-04-26 13:27:49 +0100

    added tests for .anonymise

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -13,3 +13,29 @@ test_that("is_na works as expected", {
   expect_false(is_na(list(1, 2, 3)))
   expect_false(is_na(as.Date("2020-01-01")))
 })
+
+test_that(".anonymise works as expected", {
+  set.seed(1)
+  expect_identical(.anonymise("string"), "5dMaH9wQnr")
+})
+
+test_that(".anonymise works as expected with different string_lne", {
+  set.seed(1)
+  expect_identical(.anonymise("string", string_len = 5), "5dMaH")
+})
+
+test_that(".anonymise works as expected with NAs", {
+  set.seed(1)
+  expect_identical(
+    .anonymise(c("string", NA, "string2", NA)),
+    c("5dMaH9wQnr", NA, "7YGuuP2Tjg", NA)
+  )
+})
+
+test_that(".anonymise works as expected with repeated strings", {
+  set.seed(1)
+  expect_identical(
+    .anonymise(c("string", "string2", "string")),
+    c("5dMaH9wQnr", "7YGuuP2Tjg", "5dMaH9wQnr")
+  )
+})
