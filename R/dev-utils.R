63d0bde18f86b6e2f6a6974d6acc6e0b0cb90b1e
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-05-22 12:12:54 +0100

    add contributor point to release_bullets

--- R/dev-utils.R
+++ R/dev-utils.R
@@ -4,6 +4,7 @@ release_bullets <- function() { # nocov start
   c(
     "Run `goodpractice::gp()`",
     "Review [WORDLIST](https://docs.cran.dev/spelling#wordlist)",
-    "Check if `# nolint` comments are still needed with recent lintr releases"
+    "Check if `# nolint` comments are still needed with recent lintr releases",
+    "All contributors to this release are acknowledged in some way"
   )
 } # nocov end
