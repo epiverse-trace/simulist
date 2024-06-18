41c3a08f63b07ad52df6a450776029a0b3e9af08
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-06-18 18:00:17 +0100

    add .sample_age and .format_contact_matrix functions

--- R/utils.R
+++ R/utils.R
@@ -206,3 +206,111 @@ as_function <- function(x) {
   }
   contact_infectious_period
 }
+
+#' Sample age of individuals
+#'
+#' @description
+#' The ages are sampled either uniformly (using [sample()]), or are sampled
+#' with age-structured populations when the `population_age` argument is a
+#' `<data.frame>`.
+#'
+#' @inheritParams sim_linelist
+#' @param n A single `numeric` specifying how many times to sample.
+#'
+#' @return A `numeric` vector.
+#' @keywords internal
+.sample_age <- function(index_age, population_age, n, contact_df) {
+  contact_sample_weight <- 1L
+  if (is.data.frame(contact_df)) {
+    # find which age group index case is in
+    age_groups <- apply(
+      contact_df[, c("min_age", "max_age")],
+      MARGIN = 1,
+      function(x) x[1]:x[2]
+    )
+    age_group_row <- vapply(
+      age_groups,
+      function(x, y) any(y %in% x),
+      FUN.VALUE = logical(1),
+      y = index_age
+    )
+
+    # subset matrix to index case row and replicate weight ages in each group
+    contact_df_row <- contact_df[age_group_row, ]
+    contact_sample_weight <- rep(
+      unlist(contact_df_row[, 3:ncol(contact_df)]),
+      times = lengths(age_groups)
+    )
+  }
+
+  if (is.data.frame(population_age)) {
+    age_groups <- apply(population_age, MARGIN = 1, function(x) x[1]:x[2])
+    age <- unlist(age_groups)
+    pop_sample_weight <- rep(
+      population_age$proportion,
+      times = lengths(age_groups)
+    )
+    # normalise for vector length
+    pop_sample_weight <- pop_sample_weight /
+      rep(lengths(age_groups), times = lengths(age_groups))
+  } else {
+    age <- population_age[["lower"]]:population_age[["upper"]]
+    pop_sample_weight <- 1L
+  }
+
+  # combine sample weights from contacts and population
+  sample_weight <- contact_sample_weight * pop_sample_weight
+
+  # sample age
+  age <- sample(x = age, size = n, replace = TRUE, prob = sample_weight)
+
+  # return age vector
+  age
+}
+
+
+#' Format a contact matrix into the \pkg{simulist} style and convert to
+#' `<data.frame>`
+#'
+#' @param x A contact `matrix`.
+#'
+#' @return A contact matrix `<data.frame>`.
+#' @keywords internal
+.format_contact_matrix <- function(x, population_age) {
+  # convert matrix to data.frame
+  x <- as.data.frame(x)
+  colnames(x) <- row.names(x)
+
+  # get lower and upper age limits for each group
+  age_groups <- row.names(x)
+  age_bounds <- strsplit(age_groups, split = ",", fixed = TRUE)
+  age_bounds <- lapply(
+    age_bounds,
+    gsub,
+    pattern = ".*?([0-9]+).*",
+    replacement = "\\1"
+  )
+  age_bounds <- lapply(age_bounds, as.numeric)
+  # if last element has single element (e.g. 40+) append upper age bound
+  if (length(age_bounds[length(age_bounds)]) == 1) {
+    age_bounds[[length(age_bounds)]] <- c(
+      age_bounds[[length(age_bounds)]],
+      population_age[["upper"]]
+    )
+  }
+
+  min_age <- vapply(age_bounds, "[[", FUN.VALUE = numeric(1), 1)
+  max_age <- vapply(age_bounds, "[[", FUN.VALUE = numeric(1), 2)
+  # adjust max values that are exclusive bounds
+  max_age_ <- max_age - 1
+  # oldest age group has inclusive upper bound
+  max_age_[length(max_age_)] <- max_age[length(max_age)]
+  max_age <- max_age_
+
+  # bind min and max age for each group to data.frame
+  x <- cbind(min_age = min_age, max_age = max_age, x)
+  row.names(x) <- NULL
+
+  # return contact matrix data.frame
+  x
+}
