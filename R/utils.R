09a4dad0054da2b961f3ecf7130448f84f2976c6
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-01-09 14:02:54 +0000

    add namespace to randomNames in .sample_names documentation

--- R/utils.R
+++ R/utils.R
@@ -5,8 +5,8 @@
 #' exhausting number of name when `sample.with.replacement = FALSE`. The
 #' duplicated names during sampling need to be removed to ensure each
 #' individual has a unique name. In order to have enough unique names, more
-#' names than required are sampled from [randomNames()], and the level of
-#' oversampling is determined by the `buffer_factor` argument. A
+#' names than required are sampled from [randomNames::randomNames()], and the
+#' level of oversampling is determined by the `buffer_factor` argument. A
 #' `buffer_factor` too high and the more names are sampled which takes longer,
 #' a `buffer_factor` too low and not enough unique names are sampled and
 #' the `.sample_names()` function will need to loop until it has enough
@@ -15,7 +15,7 @@
 #' @inheritParams .add_cols
 #' @param buffer_factor A single `numeric` determining the level of
 #' oversampling (or buffer) when creating a vector of unique names from
-#' [randomNames()].
+#' [randomNames::randomNames()].
 #'
 #' @return A `character` vector.
 #' @keywords internal
