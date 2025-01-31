b1be3180da247281f9dde2a4a9a79a23dd92a374
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-01-31 15:26:06 +0000

    add spelling mistakes to messy function

--- R/messy.R
+++ R/messy.R
@@ -7,12 +7,14 @@
 #' By default `messy()`:
 #'
 #' * Makes 10% of values missing, i.e. converts to `NA`.
+#' * Introduces spelling mistakes in 10% of `character` columns.
 #'
 #' To change the defaults of `messy()` arguments can be supplied to `...`.
 #' Accepted arguments and their defaults are:
 #'
 #' * `prop_missing = 0.1`
 #' * `missing_value = NA`
+#' * `prop_spelling_mistakes = 0.1`
 #'
 #' @return A messy line list `<data.frame>`.
 #' @export
@@ -23,10 +25,14 @@
 #'
 #' # increasing proportion of missingness to 30% with a missing value of -99
 #' messy_linelist <- messy(linelist, prop_missing = 0.3, missing_value = -99)
+#'
+#' # increasing proportion of spelling mistakes to 50%
+#' messy_linelist <- messy(linelist, prop_spelling_mistakes = 0.5)
 messy <- function(linelist, ...) {
   args <- list(
     prop_missing = 0.1,
-    missing_value = NA
+    missing_value = NA,
+    prop_spelling_mistakes = 0.1
   )
 
   # capture dynamic dots
@@ -41,6 +47,22 @@ messy <- function(linelist, ...) {
 
   # replace default args if in dots
   args <- utils::modifyList(args, dots)
+
+  # check args list after any user changes
+  checkmate::assert_number(args$prop_spelling_mistakes, lower = 0, upper = 1)
+
+  if (args$prop_spelling_mistakes > 0) {
+    # only apply spelling mistakes on character columns
+    char_cols <- names(linelist)[(vapply(linelist, is.character, logical(1)))]
+    linelist[char_cols] <- lapply(linelist[char_cols], function(col) {
+      misspell <- runif(length(col)) < args$prop_spelling_mistakes
+      ifelse(
+        test = misspell,
+        yes = vapply(col, .spelling_mistake, character(1)),
+        no = col
+      )
+    })
+  }
 
   # random missingness introduced across <data.frame>
   num_missing <- round(prod(dim(linelist)) * args$prop_missing)
