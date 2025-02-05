c2156e2da32b979d213da97a8dba36901ee2d8e8
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-02-05 13:19:50 +0000

    add units tests for numeric_as_char in messy()

--- tests/testthat/test-messy.R
+++ tests/testthat/test-messy.R
@@ -8,6 +8,10 @@ test_that("messy works as expected by default", {
   expect_identical(dim(ll), dim(messy_ll))
   expect_identical(colnames(ll), colnames(messy_ll))
   expect_gt(sum(is.na(messy_ll)), sum(is.na(ll)))
+  expect_false(
+    c("numeric", "integer") %in%
+      vapply(messy_ll, class, FUN.VALUE = character(1))
+  )
 })
 
 test_that("messy works with higher proportion of spelling mistakes", {
@@ -50,6 +54,30 @@ test_that("messy works without inconsistent sex", {
 test_that("messy works encoding sex as numeric", {
   messy_ll <- messy(ll, sex_as_numeric = TRUE, inconsistent_sex = FALSE)
   expect_true(all(messy_ll$sex %in% c(0L, 1L, NA_integer_)))
+})
+
+test_that("messy works without numeric_as_char", {
+  messy_ll <- messy(ll, numeric_as_char = FALSE)
+  col_class <- vapply(messy_ll, class, FUN.VALUE = character(1))
+  expect_true(c("numeric", "integer") %in% col_class)
+  expect_type(
+    col_class[c("id", "age", "ct_value")],
+    c("integer", "integer", "numeric")
+  )
+})
+
+test_that("messy works with numeric_as_char & sex_as_numeric", {
+  messy_ll <- messy(
+    ll,
+    numeric_as_char = TRUE,
+    sex_as_numeric = TRUE,
+    inconsistent_sex = FALSE
+  )
+  expect_false(
+    c("numeric", "integer") %in%
+      vapply(messy_ll, class, FUN.VALUE = character(1))
+  )
+  expect_type(messy_ll$sex, type = "character")
 })
 
 test_that("messy errors when sex_as_numeric & inconsistent_sex are TRUE", {
