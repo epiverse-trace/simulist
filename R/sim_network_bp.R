ce08bd3022015411cdde6d98eebba8a01af3df31
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-02-19 21:57:47 +0000

    use time_ var in sim_network_bp

--- R/sim_network_bp.R
+++ R/sim_network_bp.R
@@ -36,14 +36,14 @@
   ancestor <- vector(mode = "integer", 1e5)
   generation <- vector(mode = "integer", 1e5)
   infected <- vector(mode = "integer", 1e5)
-  .time <- vector(mode = "double", 1e5)
+  time_ <- vector(mode = "double", 1e5)
 
   # store initial individual
   ancestor[1] <- NA_integer_
   generation[1] <- 1L
   # 1 is infected, 0 is non-infected contact
   infected[1] <- 1L
-  .time[1] <- 0
+  time_[1] <- 0
 
   # initialise counters
   next_gen_size <- 1L
@@ -81,7 +81,7 @@
             ancestor <- c(ancestor, vector(mode = "integer", 1e5))
             generation <- c(generation, vector(mode = "integer", 1e5))
             infected <- c(infected, vector(mode = "integer", 1e5))
-            .time <- c(.time, vector(mode = "double", 1e5))
+            time_ <- c(time_, vector(mode = "double", 1e5))
           }
 
           generation[vec_idx] <- chain_generation
@@ -106,7 +106,7 @@
             min = 0,
             max = contact_infectious_period
           )
-          .time[vec_idx] <- contact_times + .time[ancestor_idx[i]]
+          time_[vec_idx] <- contact_times + time_[ancestor_idx[i]]
         }
       }
       ancestor_idx <- setdiff(which(infected == 1), prev_ancestors)
@@ -135,7 +135,7 @@
   # if the outcome names are changed, please find and update all
   # logical expressions, e.g., x == "infected" or x == "contact"
   infected <- ifelse(test = infected, yes = "infected", no = "contact")
-  .time <- .time[seq_along(generation)]
+  time_ <- time_[seq_along(generation)]
 
   # return chain as <data.frame>
   data.frame(
@@ -143,6 +143,6 @@
     ancestor = ancestor,
     generation = generation,
     infected = infected,
-    time = .time
+    time = time_
   )
 }
