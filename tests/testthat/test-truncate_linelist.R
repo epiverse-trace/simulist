dd5c13b0edd0fbc0aa8b8f55f8df5f30d2d9bdf9
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-02-25 16:53:18 +0000

    update truncate_linelist unit tests

--- tests/testthat/test-truncate_linelist.R
+++ tests/testthat/test-truncate_linelist.R
@@ -3,30 +3,40 @@ ll <- sim_linelist()
 
 test_that("truncate_linelist works as expected with defaults", {
   ll_trunc <- truncate_linelist(ll)
-  # in this example dataset only the outcome col is adjusted from truncate_linelist
+  # in this example dataset one case is removed
   expect_false(identical(ll$date_outcome, ll_trunc$date_outcome))
+  expect_gt(nrow(ll), nrow(ll_trunc))
 })
 
-test_that("truncate_linelist works as expected with modified delay", {
+test_that("truncate_linelist works as expected with modified truncation_time", {
   ll_trunc <- truncate_linelist(
-    ll,
-    delay = function(x) rlnorm(n = x, meanlog = 3, sdlog = 2)
+    linelist = ll,
+    truncation_time = 60
   )
-  # in this example dataset the df is subset
   expect_gt(nrow(ll), nrow(ll_trunc))
 })
 
-test_that("truncate_linelist works as expected with max_date", {
-  expect_message(ll_trunc <- truncate_linelist(ll, max_date = "2023-02-01"))
-  # in this example dataset the df is subset
+test_that("truncate_linelist works as expected with different units", {
+  ll_trunc <- truncate_linelist(
+    linelist = ll,
+    truncation_time = 3,
+    unit = "weeks"
+  )
   expect_gt(nrow(ll), nrow(ll_trunc))
-  expect_true(all(ll_trunc$date_onset < as.Date("2023-02-01")))
 })
 
-test_that("truncate_linelist works as expected with outcome delay_type", {
-  ll_trunc <- truncate_linelist(ll, truncation_event = "outcome")
-  # in this example dataset the df is subset
+test_that("truncate_linelist works as expected with forward direction", {
+  ll_trunc <- truncate_linelist(linelist = ll, direction = "forward")
   expect_gt(nrow(ll), nrow(ll_trunc))
+})
+
+test_that("truncate_linelist workds as expected with <Date> truncation_time", {
+  ll_trunc <- truncate_linelist(
+    linelist = ll,
+    truncation_time = as.Date("2023-03-01")
+  )
+  expect_gt(nrow(ll), nrow(ll_trunc))
+  expect_true(all(ll_trunc$date_reporting < as.Date("2023-03-01")))
 })
 
 test_that("truncate_linelist sets dates as NA when between events", {
@@ -34,13 +44,10 @@ test_that("truncate_linelist sets dates as NA when between events", {
   # hospitalisation and onset to death to truncate between reporting and event
   ll <- sim_linelist(
     hosp_risk = 0.9,
-    onset_to_hosp = function(x) stats::rlnorm(n = x, meanlog = 2, sdlog = 0.1),
+    onset_to_hosp = function(x) stats::rlnorm(n = x, meanlog = 3, sdlog = 0.1),
     onset_to_death = function(x) stats::rlnorm(n = x, meanlog = 3, sdlog = 0.1)
   )
-  ll_trunc <- truncate_linelist(
-    ll,
-    delay = function(x) rlnorm(n = x, meanlog = 2, sdlog = 0.5)
-  )
+  ll_trunc <- truncate_linelist(ll, truncation_time = 30)
   # it is possible that the proportion of NAs in the truncated data is lower
   # than the complete data if by chance the hospitalised cases are removed
   # but some NAs should be introduced in truncated data in rows that are kept
@@ -50,10 +57,22 @@ test_that("truncate_linelist sets dates as NA when between events", {
   )
 })
 
-test_that("truncate_linelist prints message as expected for with numeric max_date", {
-  expect_message(
-    truncate_linelist(ll, max_date = 10),
-    regexp = "(Truncation max date is:)*(Assuming)*(origin)*('1970-01-01')"
+test_that("truncate_linelist warns if truncation_time is Date and unit given", {
+  expect_warning(
+    truncate_linelist(
+      linelist = ll,
+      truncation_time = as.Date("2023-03-01"),
+      unit = "weeks"
+    ),
+    regexp = "(truncation_time)*(is)*(Date)*(unit)*(direction)*(are ignored)"
+  )
+  expect_warning(
+    truncate_linelist(
+      linelist = ll,
+      truncation_time = as.Date("2023-03-01"),
+      direction = "forwards"
+    ),
+    regexp = "(truncation_time)*(is)*(Date)*(unit)*(direction)*(are ignored)"
   )
 })
 
@@ -64,16 +83,23 @@ test_that("truncate_linelist fails as expected for invalid linelist", {
   )
 })
 
-test_that("truncate_linelist fails as expected for invalid delay", {
+test_that("truncate_linelist fails as expected for invalid truncation_time", {
   expect_error(
-    truncate_linelist(ll, delay = function(x, y) x + y),
-    regexp = "(delay supplied must have 1 argument)"
+    truncate_linelist(ll, truncation_time = -1),
+    regexp = "(truncation_time)*(single positive numeric or)*(Date)"
   )
 })
 
-test_that("truncate_linelist fails as expected for invalid delay_type", {
+test_that("truncate_linelist fails as expected for invalid unit", {
   expect_error(
-    truncate_linelist(ll, truncation_event = "random"),
-    regexp = "(should be one of)*(reporting)*(onset)*(admission)*(outcome)"
+    truncate_linelist(ll, unit = "daily"),
+    regexp = "(should be one of)*(days)*(weeks)*(months)*(years)"
+  )
+})
+
+test_that("truncate_linelist fails as expected for invalid direction", {
+  expect_error(
+    truncate_linelist(ll, direction = "retrospective"),
+    regexp = "(should be one of)*(backwards)*(forwards)"
   )
 })
