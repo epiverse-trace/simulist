6cb2d9fa701fd8060e60e1f86f318076c34c1cac
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-03-07 17:46:54 +0000

    update joss-figs. R script and save figure images

--- joss/joss-figs.R
+++ joss/joss-figs.R
@@ -4,7 +4,12 @@ library(dplyr)
 library(incidence2)
 library(ggplot2)
 
-set.seed(123)
+# Plot onset, admission, death incidence ----------------------------------
+
+# simulate a large outbreak to show outbreak dynamics for cases,
+# hospitaliations and deaths
+
+set.seed(1)
 linelist <- sim_linelist(outbreak_size = c(1e3, 1e4))
 
 linelist <- linelist %>%
@@ -29,10 +34,26 @@ weekly <- incidence2::incidence(
   groups = "sex",
   complete_dates = TRUE
 )
-plot(weekly)
+inci_plot <- plot(weekly)
 
-set.seed(123)
-outbreak <- sim_outbreak()
+ggplot2::ggsave(
+  filename = file.path("joss", "incidence.png"),
+  plot = inci_plot
+)
+
+
+# Plot network and events -------------------------------------------------
+
+# simulate a small outbreak with a high hospitalisation risk, reporting delay
+# distribution and onset-to-recovery delay distribution
+
+set.seed(2)
+outbreak <- sim_outbreak(
+  onset_to_recovery = function(x) stats::rlnorm(n = x, meanlog = 2, sdlog = 0.5),
+  reporting_delay = function(x) stats::rlnorm(n = x, meanlog = 1, sdlog = 0.5),
+  hosp_risk = 0.5,
+  outbreak_size = c(10, 50)
+)
 epicontacts <- make_epicontacts(
   linelist = outbreak$linelist,
   contacts = outbreak$contacts,
@@ -43,6 +64,9 @@ epicontacts <- make_epicontacts(
 )
 plot(epicontacts)
 
+# this <epicontacts> network plot is saved by manually exporting it to .png
+# from the Rstudio IDE
+
 linelist <- outbreak$linelist
 
 tidy_linelist <- linelist %>%
@@ -52,7 +76,7 @@ tidy_linelist <- linelist %>%
   mutate(ordering_value = ifelse(name == "date_onset", value, NA)) %>% # nolint consecutive_mutate_linter
   mutate(case_name = reorder(case_name, ordering_value, min, na.rm = TRUE)) # nolint consecutive_mutate_linter
 
-ggplot(data = tidy_linelist) +
+ll_plot <- ggplot(data = tidy_linelist) +
   geom_line(
     mapping = aes(x = value, y = case_name),
     linewidth = 0.25
@@ -82,4 +106,8 @@ ggplot(data = tidy_linelist) +
   theme_bw() +
   theme(legend.position = "bottom", axis.text.y = element_text(size = 4))
 
-ggplot2::ggsave(filename = file.path("joss", "simulist-joss-fig.png"))
+
+ggplot2::ggsave(
+  filename = file.path("joss", "multi-dot-plot.png"),
+  plot = ll_plot
+)
