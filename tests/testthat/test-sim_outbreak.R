0351b2136d28aa1835408b98b54446d344d79262
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-04-13 15:43:56 +0100

    update age-stratified risk data.frames minimum age to zero in  sim_linelist and sim_outbreak tests

--- tests/testthat/test-sim_outbreak.R
+++ tests/testthat/test-sim_outbreak.R
@@ -42,15 +42,15 @@ test_that("sim_outbreak works as expected with anonymised", {
 
 test_that("sim_outbreak works as expected with age-strat risks", {
   age_dep_hosp_risk <- data.frame(
-    age_limit = c(1, 5, 80),
+    age_limit = c(0, 5, 80),
     risk = c(0.1, 0.05, 0.2)
   )
   age_dep_hosp_death_risk <- data.frame(
-    age_limit = c(1, 5, 80),
+    age_limit = c(0, 5, 80),
     risk = c(0.1, 0.05, 0.2)
   )
   age_dep_non_hosp_death_risk <- data.frame(
-    age_limit = c(1, 5, 80),
+    age_limit = c(0, 5, 80),
     risk = c(0.05, 0.025, 0.1)
   )
   set.seed(1)
